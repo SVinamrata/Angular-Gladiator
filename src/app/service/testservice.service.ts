ef691c7f7df4bc5d198a0f8679b08319b71f04ea
SVinamrata <vinamratasaxena346@gmail.com>
2020-08-26 10:39:27 +0530

    Test component

--- src/app/service/testservice.service.ts
+++ src/app/service/testservice.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from "@angular/common/http";
 import { Observable } from 'rxjs';
 // import { Subject } from "../app/subjects";
+import { Exam } from "../models/exam";
 
 
 @Injectable({
@@ -12,5 +13,10 @@ export class TestserviceService {
   constructor(private http:HttpClient) { }
   fetchSubjects(){
     return this.http.get('http://localhost:8181/testsAvailable');
+  }
+  registerforExam(exam:Exam){
+    let url='http://localhost:8181/examRegister';
+    return this.http.post(url,exam);
+
   }
 }

2b73f25d4e10a779b128e33a600effa50dedec8d
VarunNightRider <Varunendr.Singh@lntinfotech.com>
2020-08-27 22:50:34 +0530

    changes

--- src/app/register/regis.component.ts
+++ src/app/register/regis.component.ts
@@ -12,6 +12,7 @@ import { Router } from '@angular/router';
 
 
 @Component({
+  
   selector: 'app-regis',
   templateUrl:'./regis.component.html', 
   styleUrls: ['./regis.component.css'
@@ -28,37 +29,9 @@ export class RegisComponent implements OnInit {
   student:Student = new Student();
   confirmPass:any="";
 
-  nameOfStudent:any;
-  stateSelected:any="Uttrakhand";
-  stateselected1:any;
   
-  districtSelected:any="Dehradun";
 
-//  empl : Employee=new Employee();
-  state:State[]=[ { Id:1,Name:"Uttrakhand"},
-  {Id:2,Name:"Uttar Pradesh"},
-  { Id:3,Name:"Madhya Pradesh"},
-  {Id:4,Name:"Rajasthan"}];
 
-  mp:MadhyaPradesh[]=[ { id:1,districtName:"Jabalpur"},
-  {id:2,districtName:"Bhopal"},
-  { id:3,districtName:"Indore"},
-  {id:4,districtName:"NarsinghPur"}];
-
-  uk:Uttrakhand[]=[ { id:1,districtName:"Dehradun"},
-  {id:2,districtName:"Roorkie"},
-  { id:3,districtName:"Haridwar"},
-  {id:4,districtName:"Garhwal"}];
-
-  up:UttarPradesh[]=[ { id:1,districtName:"Amethi"},
-  {id:2,districtName:"GorakhPur"},
-  { id:3,districtName:"Lucknow"},
-  {id:4,districtName:"Gaziabad"}];
-
-  rj:Rajasthan[]=[ { id:1,districtName:"Jaipur"},
-  {id:2,districtName:"Kota"},
-  { id:3,districtName:"Jaisalmer"},
-  {id:4,districtName:"Jodhpur"}];
 
   
 
@@ -67,68 +40,32 @@ export class RegisComponent implements OnInit {
   modifiedText: string;
 
   ngOnInit(){
-    this.student.studentState="Uttrakhand";
-    this.student.studentCity="Dehradun";
+   
    
    // this.empl.Name=this.employee.indexOf(this.empSelected)[1];
     
     
     
   }
-  isMadhyaPradesh:boolean=false;
-  isUttrakhand:boolean=true;
-  isUttarPradesh:boolean=false;
-  isRajasthan:boolean=false;
-  onStateSelected(event:any)
+  
+  onStateSelected()
   {
-    if(event=="Uttrakhand")
-    {
-      this.student.studentState="Uttrakhand";
-      this.student.studentCity="Dehradun"
-      this.isMadhyaPradesh=false;
-      this.isUttrakhand=true;
-      this.isMadhyaPradesh=false;
-      this.isRajasthan=false;
-
-    }
-    else if (event=="Madhya Pradesh") {
-      this.student.studentState="Madhya Pradesh";
-      this.student.studentCity="Jabalpur";
-      this.isMadhyaPradesh=true;
-      this.isUttrakhand=false;
-      this.isRajasthan=false;
-      this.isUttarPradesh=false;
-      
-    } 
-    else if (event=="Uttar Pradesh") {
-      this.student.studentState="UttarPradesh";
-      this.student.studentCity="Amethi";
-      this.isMadhyaPradesh=false;
-      this.isUttrakhand=false;
-      this.isUttarPradesh=true;
-      this.isRajasthan=false;
+    console.log(this.student.studentEmail);
+    console.log(this.student.studentPassword);
+   
+  
 
       
-    }
-      
-    else if(event=="Rajasthan")
-    {
-      this.student.studentState="Rajasthan";
-      this.student.studentCity="Kota";
-      this.isMadhyaPradesh=false;
-      this.isUttrakhand=false;
-      this.isUttarPradesh=false;
-      this.isRajasthan=true;
-      
-    }
+    
   }
   yearOC(event){ 
     event.preventDefault();
   }
   onDistrictSelected(event:any)
-  { 
-    this.student.studentCity=this.districtSelected;
-    this.student.studentState=this.stateSelected;
+  {
+    console.log(this.student.studentEmail);
+    console.log(this.student.studentPassword); 
+  
     
   }
   register1()
